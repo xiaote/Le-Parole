203f4980874b06566a9c47c069af09fddf7ca601
xiaote <5459500+xiaote@users.noreply.github.com>
2026-09-22 20:07:33 +0200

    Render launch UI before database startup

--- Le Parole/Le_ParoleApp.swift
+++ Le Parole/Le_ParoleApp.swift
@@ -2,11 +2,6 @@ import SwiftUI
 
 @main
 struct Le_ParoleApp: App {
-    init() {
-        // Force DatabaseService initialization on launch so migrations run before any view appears.
-        _ = DatabaseService.shared
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
